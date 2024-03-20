9f481718d0033b2bd50943b0f1bbcd3283e5ffbf
LauraGSanchz <145982662+LauraGSanchz@users.noreply.github.com>
2024-03-20 23:27:04 +0100

    20/03

--- ml_project/src/app/app.py
+++ ml_project/src/app/app.py
@@ -1,65 +1,72 @@
 """""archivo donde corre la app"""
 from flask import Flask, request, render_template,jsonify
-import pickle
 import os
 import seaborn as sns
 import pandas as pd
+from joblib import  load
+import statsmodels.api as sm
+
+app = Flask(__name__, static_url_path='/static')
 
 
-app = Flask(__name__)
+modelo = None
+modelo_ruta = 'C:\\Users\\laura\\OneDrive\\Desktop\\PROYECTO ML\\ml_project\\src\\models\\ignore\\exogen_model3.joblib'
 
-# Ruta al archivo del modelo
-modelo_ruta = 'C:\\Users\\laura\\OneDrive\\Desktop\\PROYECTO ML\\ml_project\\src\\models\\ignore\\forecaster_model.pkl'
-
-# Verificar si el archivo existe antes de cargarlo
-if os.path.exists(modelo_ruta):
-    with open(modelo_ruta, 'rb') as archivo_modelo:
-        modelo = pickle.load(archivo_modelo)
-else:
-    print("El archivo del modelo no existe en la ruta especificada.")
+def cargar_modelo():
+    global modelo
+    if os.path.exists(modelo_ruta):
+        with open(modelo_ruta, 'rb') as archivo_modelo:
+            modelo = load(archivo_modelo)
+    else:
+        print("El archivo del modelo no existe en la ruta especificada.")
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-
 @app.route('/predict_form')
 def prediction_form():
     return render_template('predict.html')
 
-
+@app.route('/exploracion', methods=['GET'])
+def exploracion():
+    return render_template('explo.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    steps = request.form.get('steps')  # Obtener el número de pasos del formulario
+    global modelo
     
-    if steps is None:
-        return jsonify({'error': 'No se proporcionó el número de pasos.'}), 400
+    # Obtener el número de pasos del formulario
+    steps = request.form.get('steps')
+    
+    # Obtener el archivo CSV exógeno del formulario
+    file = request.files['data']
+    
+    # Verificar si todos los campos requeridos están presentes
+    if None in (steps, file):
+        return jsonify({'error': 'Faltan campos requeridos en el formulario.'}), 400
     
     # Convertir los pasos a un entero
     steps = int(steps)
     
-    # Ahora, utiliza el número de pasos recibido para realizar la predicción
-    predictions = modelo.predict(steps=steps)
+    # Leer el archivo CSV como DataFrame
+    try:
+        df_data = pd.read_csv(file)
+        df_data = pd.DataFrame(df_data)
+    except Exception as e:
+        return jsonify({'error': 'Error al leer el archivo CSV: ' + str(e)}), 400
     
-    # Devolver los resultados como una respuesta JSON
-    return jsonify({'predictions': list(predictions)})
-
-
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     data = request.json
-#     steps = int(data.get('steps'))  # Suponiendo que recibes el número de pasos como 'steps' en los datos JSON
+    # Cargar el modelo si no se ha cargado aún
+    if modelo is None:
+        cargar_modelo()
     
-#     if steps is None:
-#         return jsonify({'error': 'No se proporcionó el número de pasos.'}), 400
-    
-#     # Ahora, utiliza el número de pasos recibido para realizar la predicción
-#     predictions = modelo.predict(steps=steps)
-    
-#     return print(pd.DataFrame(predictions))
-
+    if modelo is not None:
+        # Ahora, utiliza el número de pasos recibido y los datos exógenos para realizar la predicción
+        predictions = modelo.predict(steps=steps, exog=df_data[['GHI', 'Festivo', 'Gas', 'PotenciaViento']])
+        # Devolver los resultados como una respuesta JSON
+        return jsonify({'predictions': list(predictions)})
+    else:
+        return jsonify({'error': 'El modelo no pudo ser cargado.'}), 500
 
 
 
